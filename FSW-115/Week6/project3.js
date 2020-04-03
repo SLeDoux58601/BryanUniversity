278ea213874146888882aeeb5efce95f014f650a
Samantha R LeDoux <56769518+SLeDoux58601@users.noreply.github.com>
2020-04-02 20:08:12 -0600

    update assignments

--- FSW-115/Week6/project3.js
+++ FSW-115/Week6/project3.js
@@ -1,53 +1,64 @@
-const button = document.createElement("button")
-document.body.appendChild(button)
-button.textContent="StarWars API"
-button.addEventListener("click",()=>{
-    console.log (button)
+
+
     axios.get("https://swapi.co/api/people") .then(response=>{
         console.log (response.data)
+        const div = document.createElement('div')
+        div.className="StarWarsPeople"
+    document.body.appendChild(div)
+    //     const h1=document.createElement("h1")
+    //    div.append(h1)
+        const h2=document.createElement("h2")
+     div.append(h2)
+     h2.textContent="StarWars People"
+     h2.style.fontSize="30px"
+     
+    //  const div = document.createElement('div')
+    // document.body.appendChild(div)
         for (i = 0; i < response.data.results.length; i++) {
             const ul = document.createElement('ul')
 
-    document.body.appendChild(ul)
+    div.appendChild(ul)
     const li = document.createElement('li')
     li.textContent = response.data.results[i].name
     ul.appendChild(li)
           }
-    })
-})
-
-const button = document.createElement("button")
-document.body.appendChild(button)
-button.textContent=" API"
-button.addEventListener("click",()=>{
-    console.log (button)
-    axios.get("") .then(response=>{
+    }).then(response=>{axios.get("https://swapi.co/api/planets") .then(response=>{
         console.log (response.data)
+    //     const div = document.createElement('div')
+    // document.body.appendChild(div)
         for (i = 0; i < response.data.results.length; i++) {
             const ul = document.createElement('ul')
 
-    document.body.appendChild(ul)
+    div.appendChild(ul)
     const li = document.createElement('li')
     li.textContent = response.data.results[i].name
     ul.appendChild(li)
           }
-    })
-})
-
-const button = document.createElement("button")
-document.body.appendChild(button)
-button.textContent="Rick and Morty Api"
-button.addEventListener("click",()=>{
-    console.log (button)
-    axios.get("https://rickandmortyapi.com/api/character/80") .then(response=>{
+    }) .then(response=>{axios.get("https://rickandmortyapi.com/api/character") .then(response=>{
         console.log (response.data)
+        console.log(document.getElementsByClassName("RickandMorty")[0])
         for (i = 0; i < response.data.results.length; i++) {
             const ul = document.createElement('ul')
-
-    document.body.appendChild(ul)
+   div.appendChild(ul)
     const li = document.createElement('li')
     li.textContent = response.data.results[i].name
     ul.appendChild(li)
           }
     })
-})
+    const div = document.createElement('div')
+    div.className="RickandMorty"
+document.body.appendChild(div)
+    const h1=document.createElement("h1")
+   div.append(h1)
+   
+    h1.textContent="Rick & Morty Api"
+    h1.style.fontSize="30px"
+    })
+    const div = document.createElement('div')
+    div.className="Planets"
+document.body.appendChild(div)
+    const h2=document.createElement("h2")
+   div.append(h2)
+    h2.textContent="StarWars Planets"
+    h2.style.fontSize="30px"
+    })
--- FSW-115/Week7/index.js
+++ FSW-115/Week7/index.js
@@ -1,29 +1,29 @@
-async function getData(){
+ function getData(){
     
-    try {
-        const response = await axios.get("https://api.vschool.io/zachfarmer/todo") 
-        const description = await axios.get(response.data.description)
-        const price = await axios.get(response.data.price)
-        listToDoToDOM(description, response, price)
-    }
-    catch(error){
+   axios.get("https://api.vschool.io/zachfarmer/todo") .then(response=>{
+       // const description = (response.data.description)
+       // const price = (response.data.price)
+        listToDoToDOM( response)
+    })
+    .catch(error=>{
         console.log(error)
-    }   
+    })
 }
 
 getData()
 
-function listToDoToDOM(description, response, price){
+function listToDoToDOM( response){
+    console.log (response.data)
     const nameh1 = document.createElement('h1')
     nameh1.textContent = response.data.name
     document.body.appendChild(nameh1)
 
     const h1 = document.createElement('h1')
-    h1.textContent = description.data.name
+    h1.textContent = response.data.name
     document.body.appendChild(h1)
 
     const speciesh1 = document.createElement('h2')
-    speciesh1.textContent = price.data.name
+    speciesh1.textContent = response.data.name
     document.body.appendChild(speciesh1)
     
 }
